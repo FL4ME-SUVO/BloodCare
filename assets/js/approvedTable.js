c5b0f96668e5ed3dae4e70f412019207ecf667ec
PlanCraft <shuvogit9932github@gmail.com>
2024-11-05 20:05:12 +0530

    tables updated

--- assets/js/approvedTable.js
+++ assets/js/approvedTable.js
@@ -1,16 +1,16 @@
 /*=============Approved Request Table==========================*/
     
 var clients = [
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
-    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity(Unit)": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
+    { "Donor Name": "Shubhajit Giri", "Disease" : "Yes", "Age": 22, "Blood Group": "O+", "Quantity": 2, "Request Date": "2025-04-09" },
    
     
 ];
@@ -31,7 +31,7 @@ $("#jsGrid2").jsGrid({
         { name: "Disease", type: "text", width: 80, validate: "required" },
         { name: "Age", type: "number", width: 50 , validate: "required" },
         { name: "Blood Group", type: "text", width: 50, validate: "required" },
-        { name: "Quantity(Unit)", type: "number", width:100, validate: "required" },
+        { name: "Quantity", type: "number", width:100, validate: "required" },
         { name: "Request Date", type: "date", width: 100, validate: "required"  }
        
     ]
